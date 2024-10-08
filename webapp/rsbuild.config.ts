3b304e0b8465ea8d1d60a4cf89a744905d056c38
kaje94 <a.kajendran@gmail.com>
2024-10-08 17:11:48 +0530

    add bal svc

--- webapp/rsbuild.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "@rsbuild/core";
-import { pluginReact } from "@rsbuild/plugin-react";
-
-export default defineConfig({
-  html: { title: "Notes App" },
-  plugins: [pluginReact()],
-});
